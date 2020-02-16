8ffdbd66230c4f356bc1dfc4dbd4efbc945a4ada
Łukasz Kusiak <lukaszkusiak@MacBook-Pro-ukasz.local>
2020-02-16 13:56:44 +0100

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
+import { Id } from './id';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'id-sk-gen';
+
+  model = new Id("Specimen", "Vzorka", "SVK", "F","EA000000", "11.11.1911", "Bratislava", "111111/111", "01.03.2025", "01.03.2015",  "Bratislava", "Zizkova 12", "", "Povodna", "Banska Bystrica", "Mgr.", "IDSVKEA000000<81111111111<<<<<", "1111112F2503011SVK<<<<<<<<<<<6", "SPECIMEN<<VZORKA<<<<<<<<<<<<<<")
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  @ViewChild('fileInput') el: ElementRef;
+  imageUrl: any = "../assets/face.png";
+  editFile: boolean = true;
+  removeUpload: boolean = false;
+
+  uploadFile(event) {
+    let reader = new FileReader(); // HTML5 FileReader API
+    let file = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      reader.readAsDataURL(file);
+
+      // When file uploads set it to file formcontrol
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+        this.editFile = false;
+        this.removeUpload = true;
+      }
+      // ChangeDetectorRef since file is loading outside the zone
+      //this.cd.markForCheck();        
+    }
+  }
+
+  // Function to remove uploaded file
+  removeUploadedFile() {
+    let newFileList = Array.from(this.el.nativeElement.files);
+    this.imageUrl = 'https://i.pinimg.com/236x/d6/27/d9/d627d9cda385317de4812a4f7bd922e9--man--iron-man.jpg';
+    this.editFile = true;
+    this.removeUpload = false;
+    // this.registrationForm.patchValue({
+    //   file: [null]
+    // });
+  }
 }
---
+++ src/app/id.ts
@@ -0,0 +1,25 @@
+export class Id {
+
+    constructor(
+      public name: string,
+      public surname: string,
+      public nationality: string,
+      public sex: string,
+      public idNumber: string,
+      public dateOfBirth: string,
+      public issuedBy: string,
+      public personalNo: string,
+      public dateOfExpiry: string,
+      public dateOfIssue: string,
+      public addressline1: string,
+      public addressline2: string,
+      public addressline3: string,
+      public surnameAtBirth: string,
+      public placeOfBirth: string,
+      public specialRemarks: string,
+      public mrzData1 : string,
+      public mrzData2: string,
+      public mrzData3: string
+    ) {  }
+  
+  }
